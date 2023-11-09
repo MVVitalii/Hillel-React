a34986d690f11452518f9e186e3f38d361c0b989
Vitalii <mykhailiuchenko.vitalii@gmail.com>
2023-11-09 15:01:40 +0200

    React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Title from './components/Title/Title';
+
+function App() {
+  return (
+    <div>
+      <Title text="First component" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,5 @@
+const Title = (props) => {
+  // eslint-disable-next-line react/prop-types
+  return <h1>{props.text}</h1>;
+};
+export default Title;
